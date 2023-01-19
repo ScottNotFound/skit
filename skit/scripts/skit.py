d9a7dceac4a915c103b188e46e8a44379dc32ba1
Scott Thiel <sthiel@umass.edu>
2023-01-19 15:56:12 +0000

    add skit script to list scripts

---
+++ skit/scripts/skit.py
@@ -0,0 +1,10 @@
+
+scripts = ["ssub"]
+
+def main():
+
+    print("This package includes the following scripts:")
+    for script in scripts:
+        print(script)
+    print()
+    print("See the individual scripts for usage.")
